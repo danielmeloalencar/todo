8f6318c6334a06c68da0cd26482840e2f037f6b4
Daniel Alencar <danielmalencar2011@gmail.com>
2022-08-03 22:21:57 -0300

    Type Item

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { useState } from 'react';
 import * as C from './App.styles';
+import { Item } from './types/Item'
+
 
 const App = () => {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'Compar o pão na padaria', done: false },
+    { id: 2, name: 'Compar um bolo na padaria', done: false },
+  ]);
+
   return (
     <C.Container>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
+
+        {list.map((item, index) => (
+          <div>{item.name}</div>
+        ))}
+
+
       </C.Area>
     </C.Container>
   );
