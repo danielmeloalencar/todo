739259eb81bed92b7ab144d2be71af848bbf84ea
Daniel Alencar <danielmalencar2011@gmail.com>
2022-08-04 02:35:47 -0300

    Drag and Drop

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -4,6 +4,14 @@ export const Container = styled.div`
     background-color:  #17181F;
     color: #797a81;
     min-height: 100vh;
+    -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+    -khtml-user-select: none; /* Konqueror HTML */
+    -moz-user-select: none; /* Old versions of Firefox */
+    -ms-user-select: none; /* Internet Explorer/Edge */
+    user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
+
 `
 
 export const Area = styled.div`
--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,44 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import * as C from './App.styles';
 import { Item } from './types/Item';
 import { ListItem } from './components/ListItem';
 import { AddArea } from './components/addArea';
 
 const App = () => {
+
+  const dragItem =useRef<any>();
+  const dragOverItem = useRef<any>();
+
   const [list, setList] = useState<Item[]>([
     {id:0, name: 'Marcar como concluído', done:true},
     {id:1, name: 'Remover tarefa', done:true},
-    {id:2, name: 'Persistir os dados com AsyncStorage', done:false},
-    {id:3, name: 'Exibir mensagem de confirmação ao delelar', done:false},
-    {id:4, name: 'Ordenar com drag and drop', done:false},
+    {id:2, name: 'Ordenar com drag and drop', done:true},
+    {id:3, name: 'Persistir os dados com AsyncStorage', done:false},
+    {id:4, name: 'Exibir mensagem de confirmação ao delelar', done:false},
     {id:5, name: 'Melhorar UI', done:false}
   ]);
 
-  const handleAddTask = (taskName: string) => {
+   
+  const dragStart = (position:number):void => {
+    dragItem.current = position;
+  };
+
+
+  const dragEnter = (position:number): void => {
+    dragOverItem.current = position;
+  };
+
+  const drop = (e: React.DragEvent) => {
+    const copyListItems = [...list];
+    const dragItemContent = copyListItems[dragItem.current];
+    copyListItems.splice(dragItem.current, 1);
+    copyListItems.splice(dragOverItem.current, 0, dragItemContent);
+    dragItem.current = null;
+    dragOverItem.current = null;
+    setList(copyListItems);
+  };
+
+  const handleAddTask = (taskName: string):void=> {
     let newList = [...list];
     newList.push({
       id: list.length + 1,
@@ -25,7 +49,7 @@ const App = () => {
     setList(newList);
   }
 
-  const handleOnChange = (id: number, done: boolean) => {
+  const handleOnChange = (id: number, done: boolean):void => {
     let newList = list.map((item, index) => {
       if (item.id === id) {
         item.done = done;
@@ -36,7 +60,7 @@ const App = () => {
     setList(newList);
   }
 
-  const handleOnRemove = (id: number) => {
+  const handleOnRemove = (id: number):void => {
     let newList = list.filter((item)=>item.id !== id)
     setList(newList);
   }
@@ -49,12 +73,21 @@ const App = () => {
         <AddArea onEnter={handleAddTask} />
 
         {list.map((item, index) => (
-          <ListItem 
+          <div 
             key={index} 
+            draggable 
+            ref={dragItem} 
+            onDragStart={e => dragStart(index)} 
+            onDragEnter={e => dragEnter(index)} 
+            onDragEnd={drop}
+            style={{cursor:'grab'}}
+          >
+          <ListItem 
             item={item} 
             onChange={handleOnChange}
             onRemove={handleOnRemove}
             />
+            </div>
         ))}
 
 
