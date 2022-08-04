f560ebb187b12b76f6d862bd5a5f8f785e779114
Daniel Alencar <danielmalencar2011@gmail.com>
2022-08-03 23:08:20 -0300

    Adicionar nova tarefa

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,28 @@ import { useState } from 'react';
 import * as C from './App.styles';
 import { Item } from './types/Item';
 import { ListItem } from './components/ListItem';
+import { AddArea } from './components/addArea';
 
 const App = () => {
-  const [list, setList] = useState<Item[]>([
-    { id: 1, name: 'Compar o pão na padaria', done: false },
-    { id: 2, name: 'Compar um bolo na padaria', done: true },
-  ]);
+  const [list, setList] = useState<Item[]>([]);
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    })
+
+    setList(newList);
+  }
 
   return (
     <C.Container>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
+
+        <AddArea onEnter={handleAddTask}/>
 
         {list.map((item, index) => (
           <ListItem key={index} item={item}></ListItem>
